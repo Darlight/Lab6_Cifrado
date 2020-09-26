a90e40600eb30adeddc9a6cd9325737f9549b7f5
JosueS22 <35278085+JosueS22@users.noreply.github.com>
2020-09-26 15:44:03 -0600

    Update lib_RSA.py

---
+++ lib_RSA.py
@@ -0,0 +1,22 @@
+import rsa
+
+p = int(input("Ingrese el numero p: "))
+q = int(input("Ingrese el numero q: "))
+m = input("Ingrese el mensaje a encriptar: ")
+
+# Se generan las llaves de bob
+(bob_public_key, bob_private_key) = rsa.newkeys(256)
+print("Llave publica de Bob: ", bob_public_key.n)
+print("Llave privada de Bob: ", bob_private_key.n)
+
+# El modulo de RSA solo opera con bytes, no con strings
+message = m.encode('utf8')
+print("Mensaje: ", message)
+
+# Alice encripta el mensaje usando la llave publica de Bob
+crypto = rsa.encrypt(message, bob_public_key)
+print("Mensaje encriptado: ", crypto)
+
+#Bob recibe el mensaje y lo decripta con su llave privada
+message = rsa.decrypt(crypto, bob_private_key)
+print("Mensaje descifrado: ", message)
